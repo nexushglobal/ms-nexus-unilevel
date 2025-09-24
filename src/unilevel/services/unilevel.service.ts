663b4e7b26dfd709fe2dd6c43da1cf0c57ffbe0f
JosmenDev <josmendev@gmail.com>
2025-09-24 17:55:17 -0500

    feat(docker-compose): added ports

--- src/unilevel/services/unilevel.service.ts
+++ src/unilevel/services/unilevel.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { HttpAdapter } from 'src/common/interfaces/http-adapter.interface';
 import { ProjectListResponseDto } from './../interfaces/project-list.dto';
